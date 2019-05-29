b9f51bb284b00a619073db14ad86c245d723afaa
Bayard Junior <sobreira.b@gmail.com>
2019-05-28 22:28:11 -0300

    Configurando o projeto

--- core/src/main/java/brincando/spring5/Main.java
+++ core/src/main/java/brincando/spring5/Main.java
@@ -2,13 +2,32 @@ package brincando.spring5;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
+    private static final String CONFIG_LOCATION = "beans.xml";
     public static void main(String[] args) {
 
         LOGGER.info("Guess the number");
+
+        //create context (container)
+
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+        //call method next to get a random number
+
+        int number = numberGenerator.next();
+
+        //log generated number
+        LOGGER.info("numer = {}", number);
+
+        //close context (container)
+        context.close();
     }
 }
