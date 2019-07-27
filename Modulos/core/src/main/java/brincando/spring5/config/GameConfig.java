21720a836d4e925e3a9be0a08b260e3b243ca25a
Bayard Junior <sobreira.b@gmail.com>
2019-07-27 16:10:32 -0300

    Adicionando arquivo de propriedade Setando atributo com o valor da propiedade Adicionando valor default, caso a propriedade nao exista.

--- Modulos/core/src/main/java/brincando/spring5/config/GameConfig.java
+++ Modulos/core/src/main/java/brincando/spring5/config/GameConfig.java
@@ -2,17 +2,24 @@ package brincando.spring5.config;
 
 import brincando.spring5.GuessCount;
 import brincando.spring5.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // == fields ==
 
-    private int maxNumber = 100;
+    //If properties game.maxNumber not found, than 20 is default value.
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
 
-    private int guessCount = 10;
+    //If properties game.guessNumber not found, than 5 is default value.
+    @Value("${game.guessNumber:5}")
+    private int guessCount;
 
     // == beans methods ==
 
