2db6a7e143c6307b9cd7b1d2b632d76adce0885a
Bayard Junior <sobreira.b@gmail.com>
2019-07-28 21:14:10 -0300

    Setup Dispatcher Servlet Retirando tag <dependencyManagement> Adicionando dependencia do servlet

---
+++ todo-list/src/main/java/brincando/spring5/config/WebConfig.java
@@ -0,0 +1,11 @@
+package brincando.spring5.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "brincando.spring5")
+public class WebConfig {
+}
