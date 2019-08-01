d81669ece6812c02430c7102c82dbf84a31c933a
Bayard Junior <sobreira.b@gmail.com>
2019-08-01 00:33:59 -0300

    Implementando post redirect get pattern

--- todo-list/src/main/java/brincando/spring5/controller/TodoItemController.java
+++ todo-list/src/main/java/brincando/spring5/controller/TodoItemController.java
@@ -6,12 +6,17 @@ import brincando.spring5.Util.ViewNames;
 import brincando.spring5.model.TodoData;
 import brincando.spring5.model.TodoItem;
 import brincando.spring5.service.TodoItemService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDate;
+
+@Slf4j
 @Controller
 public class TodoItemController {
 
@@ -35,8 +40,17 @@ public class TodoItemController {
         return ViewNames.ITEMS_LIST;
     }
 
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEditItem(Model model){
+        TodoItem todoItem = new TodoItem("", "", LocalDate.now());
+        model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
+        return ViewNames.ADD_ITEM;
+    }
+
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem){
+        log.info("todoItem from form = {}", todoItem);
+        todoItemService.addItem(todoItem);
         return "redirect:/" + Mappings.ITEMS;
     }
 }
