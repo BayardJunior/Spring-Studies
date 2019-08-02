8e47218bcfff84e9cab70d0a1d814b8fd77e85d5
Bayard Junior <sobreira.b@gmail.com>
2019-08-01 22:30:46 -0300

    Criaçao da tela Home Implementando navegação entre as telas usando hiperlinks

--- todo-list/src/main/java/brincando/spring5/config/WebConfig.java
+++ todo-list/src/main/java/brincando/spring5/config/WebConfig.java
@@ -1,17 +1,20 @@
 package brincando.spring5.config;
 
+import brincando.spring5.Util.ViewNames;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "brincando.spring5")
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     // == constants ==
     public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
@@ -29,4 +32,9 @@ public class WebConfig {
         return urlBasedViewResolver;
     }
 
+    // will be redirect into home view, very used when no need to user controller.
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
 }
--- todo-list/src/main/webapp/WEB-INF/view/items_list.jsp
+++ todo-list/src/main/webapp/WEB-INF/view/items_list.jsp
@@ -1,4 +1,5 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="brincando.spring5.Util.Mappings" %>
 
 <html>
 <head>
@@ -7,6 +8,10 @@
 <body>
 
     <div align="center">
+
+        <c:url var="addUrl" value="${Mappings.ADD_ITEM}"/>
+        <a href="${addUrl}">New Item</a>
+
         <table border="1" cellpadding="5">
             <caption>
                 <h2>Todo Items</h2>
@@ -15,12 +20,19 @@
             <tr>
                 <th>Title</th>
                 <th>DeadLine</th>
+                <th>Delete</th>
             </tr>
 
             <c:forEach var="item" items="${todoData.items}">
+
+                <c:url var="deleteUrl" value="${Mappings.DELETE_ITEM}">
+                    <c:param name="id" value="${item.id}"/>
+                </c:url>
+
                 <tr>
                     <td><c:out value="${item.title}"/></td>
                     <td><c:out value="${item.deadLine}"/></td>
+                    <td><a href="${deleteUrl}">Delete</a></td>
                 </tr>
             </c:forEach>
         </table>
